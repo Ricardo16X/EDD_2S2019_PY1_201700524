3722e4b5ca70c46245c70564428fc46498842814
Ricardo <perezricardo11@yahoo.com>
2019-09-05 02:47:14 -0600

    estructura_40%

--- librerias/estructuras.h
+++ librerias/estructuras.h
@@ -1,39 +1,128 @@
-#include <string>
+#include <cstdlib>
 #include <iostream>
 #include <sstream>
-#include <cstdlib>
+#include <string>
 
 /*  Definición de Estructuras   */
-struct nodoCabecera{
-    // Valores de estructura
-    std::string color;
-    int fila;
-    // apuntadores
-    nodoCabecera *sig;
-}primerNodo;
-
-struct cabecera{
-    // datos
-    int column;
-    // apuntadores
-    nodoCabecera primer_nodoCabecera;
-}primerCabecera;
-
-struct capa{
-    cabecera primer_Cabecera;
-}primerCapa;
-
-struct cubo
-{
-    // datos del cubo
-    std::string nombre;
-    // enlace a la primera capa
-    capa primer_Capa;
-    // punteros para arbol binario
-    cubo *izq;
-    cubo *der;
+struct elementoCabecera {
+  // Valores de estructura
+  std::string color;
+  int fila;
+  // apuntadores
+  elementoCabecera *sig_elementoCabecera;
 };
 
-/*  Definición de Métodos   */
+struct cabecera {
+  // datos
+  int column;
+  // apuntadores
+  elementoCabecera *primerElementoCabecera = new elementoCabecera();
+  cabecera *sigCabecera;
+};
 
-/*Construcción de Métodos*/
+struct capa {
+  std::string nombreCapa;
+  cabecera *primer_Cabecera = new cabecera();
+  // apuntadores
+  capa *sigCapa;
+};
+
+struct cubo {
+  // datos del cubo
+  std::string nombre;
+  int wImg, hImg;
+  int wPix, hPix;
+  // enlace a la primera capa
+  capa *primerCapa = new capa();
+  // punteros para arbol binario
+  cubo *izq_Cubo;
+  cubo *der_Cubo;
+} * primerCubito;
+
+/*Definición de métodos*/
+/*Arbol*/
+void agregarCubo(cubo *, cubo *);
+void desplegarImagenes(cubo);
+/*Lista Simple*/
+void agregarCapa(cubo *, capa *);
+void agregarCabecera(capa *, cabecera *);
+void agregarFila(cabecera *, elementoCabecera *);
+
+/*Estructura de árbol para el manejo del cubo*/
+void agregarCubo(cubo *raizArbol, cubo *nuevoCubo) {
+  if (nuevoCubo->nombre.compare(raizArbol->nombre) <
+      0) {  // Si el nombre de la nueva imagen es < al de la raiz, se coloca a
+            // la izquierda de la raiz.
+    agregarCubo(raizArbol->izq_Cubo, nuevoCubo);
+  } else if (nuevoCubo->nombre.compare(raizArbol->nombre) >
+             0) {  // Si el nombre de la nueva imagen es > al de la raiz, se
+                   // coloca a la derecha de la raiz.
+    agregarCubo(raizArbol->der_Cubo, nuevoCubo);
+  } else if (raizArbol ==
+             NULL) {  // Si se llega al punto en dónde el nodo del arbol no
+                      // apunta a nada, allí se colocará la nueva imagen.
+    if (primerCubito == NULL) {
+      primerCubito = nuevoCubo;
+    }
+    raizArbol = nuevoCubo;
+  }
+}
+
+void desplegarImagenes(cubo *raizArbol) {
+  // In Order
+  if (raizArbol != NULL) {
+    desplegarImagenes(raizArbol->izq_Cubo);
+    std::cout << "* " << raizArbol->nombre << " " << std::endl;
+    desplegarImagenes(raizArbol->der_Cubo);
+  }
+}
+
+/*Agregar capas al cubo actual*/
+void agregarCapa(cubo *cubito, capa *capita) {
+  if (cubito->primerCapa == NULL) {
+    cubito->primerCapa = capita;
+  } else {
+    capa *temp = cubito->primerCapa;
+    while (temp->sigCapa != NULL) {
+      temp = temp->sigCapa;
+    }
+    temp->sigCapa = capita;
+  }
+}
+
+capa obtenerCapa(cubo *cubito, std::string nombreCapa){
+  capa *temp = cubito->primerCapa;
+  while (temp->sigCapa != NULL && temp->nombreCapa != nombreCapa)
+  {
+    temp = temp->sigCapa;
+  }
+  return *temp;
+}
+
+/*Agregar cabecera a capa*/
+void agregarCabecera(capa *capita, cabecera *cabecera_chiquita) {
+  if (capita->primer_Cabecera == NULL) {
+    capita->primer_Cabecera = cabecera_chiquita;
+  } else {
+    cabecera *temp = capita->primer_Cabecera;
+    while(temp->sigCabecera != NULL){
+      temp = temp->sigCabecera;
+    }
+    temp = cabecera_chiquita;
+  }
+}
+
+/*Agregar un elemento de fila a la cabecera hecha*/
+void agregarFila(cabecera *cabecera_chiquita, elementoCabecera *elemento){
+  if (cabecera_chiquita->primerElementoCabecera == NULL)
+  {
+    cabecera_chiquita->primerElementoCabecera = elemento;
+  }else{
+    elementoCabecera *temp = cabecera_chiquita->primerElementoCabecera;
+    while (temp->sig_elementoCabecera != NULL)
+    {
+      temp = temp->sig_elementoCabecera;
+    }
+    temp = elemento;
+  }
+}
--- menu.cpp
+++ menu.cpp
@@ -1,67 +1,79 @@
 #include <iostream>
 #include "librerias/estructuras.h"
+#include "librerias/manejoCSV.h"
 
-int main(int argc, char const *argv[])
-{
-    cubo raiz = cubo();
-    /*Menú Inicial*/
-    int op = 0;
-    while (op != 7)
-    {
-        system("cls");
-        std::cout << "\t Menu" << std::endl;
-        std::cout << "1. Insertar Imagen" << std::endl;
-        std::cout << "2. Seleccionar Imagen" << std::endl;
-        std::cout << "3. Aplicar Filtros" << std::endl;
-        std::cout << "4. Edicion Manual" << std::endl;
-        std::cout << "5. Exportar Imagen" << std::endl;
-        std::cout << "6. Reportes" << std::endl;
-        std::cout << "7. Salir" << std::endl;
+int main(int argc, char const *argv[]) {
+  // Elemento para poder crear el árbol binario de cubos.
+  cubo raiz = cubo();
+  cubo temporal = cubo();
+  /*Menú Inicial*/
+  int op = 0;
+  while (op != 7) {
+    system("cls");
+    std::cout << "\t Menu" << std::endl;
+    std::cout << "1. Insertar Imagen" << std::endl;
+    std::cout << "2. Seleccionar Imagen" << std::endl;
+    std::cout << "3. Aplicar Filtros" << std::endl;
+    std::cout << "4. Edicion Manual" << std::endl;
+    std::cout << "5. Exportar Imagen" << std::endl;
+    std::cout << "6. Reportes" << std::endl;
+    std::cout << "7. Salir" << std::endl;
 
-        try
-        {
-            scanf("%i", &op);
-            switch (op)
-            {
-            case 1:
-                /*Cargar Imagen*/
-                cargarImagen(&raiz);
-                break;
-            case 2:
-                // Seleccionar Imagen
-            	break;
-            case 3:
-                // Aplicar filtros
-                break;
-            case 4:  // Edicion Manual
-                break;
-            case 5:  // Exportar Imagen
-                break;
-            case 6:  // Reportes
-                break;
-            case 7:  // Salir
-                system("cls");
-                std::cout << "\n\n#########################################" << std::endl;
-                std::cout << "#\tPHOTOGEN++ - 201700524\t\t#" << std::endl;
-                std::cout << "#########################################\n\n" << std::endl;
-                system("pause");
-                break;
-            default:
-                std::cout << "\t\nOpcion Incorrecta, Elige una opcion del menu.\n";
-                std::cin.clear();
-                std::cin.ignore(1000, '\n');
-                system("pause");
-                break;
-            }
-        }
-        catch (const std::exception &e)
-        {
-            std::cout << "\t\nOpcion Incorrecta, Elige una opcion del menu.\n";
-            std::cin.clear();
-            std::cin.ignore(1000, '\n');
-            system("pause");
-            op = 0;
-        }
+    try {
+      scanf("%i", &op);
+      switch (op) {
+        case 1:
+          /*Cargar Imagen*/
+          //cargarImagen(&raiz);
+          break;
+        case 2:
+          // Seleccionar Imagen
+          // mostrarArchivos();
+          // temporal = seleccionImagen(&raiz);
+          break;
+        case 3:
+          // Aplicar filtros
+          if (temporal.nombre.compare("") == 0) {
+            std::cout << "### No hay imagen cargada :( ###" << std::endl;
+          } else {
+          }
+          // aplicarFiltro(temporal); // temporal solamente servirá como valor,
+          // no como referencia;
+          break;
+        case 4:  // Edicion Manual
+          if (temporal.nombre.compare("") == 0) {
+            std::cout << "### No hay imagen cargada :( ###" << std::endl;
+          } else {
+          }
+          break;
+        case 5:  // Exportar Imagen
+          break;
+        case 6:  // Reportes
+            //menuReportes();
+          break;
+        case 7:  // Salir
+          system("cls");
+          std::cout << "\n\n#########################################"
+                    << std::endl;
+          std::cout << "#\tPHOTOGEN++ - 201700524\t\t#" << std::endl;
+          std::cout << "#########################################\n\n"
+                    << std::endl;
+          system("pause");
+          break;
+        default:
+          std::cout << "\t\nOpcion Incorrecta, Elige una opcion del menu.\n";
+          std::cin.clear();
+          std::cin.ignore(1000, '\n');
+          system("pause");
+          break;
+      }
+    } catch (const std::exception &e) {
+      std::cout << "\t\nOpcion Incorrecta, Elige una opcion del menu.\n";
+      std::cin.clear();
+      std::cin.ignore(1000, '\n');
+      system("pause");
+      op = 0;
     }
-    return 0;
+  }
+  return 0;
 }
